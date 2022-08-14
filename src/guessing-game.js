e8bb294d2e494b07512bd1db83387164e8b8b663
Anton <deftume@google.com>
2022-08-15 01:08:23 +0300

    feat: tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.medium = Math.round((this.min + this.max) / 2);
+        return this.medium;
     }
 
     lower() {
-
+        this.max = this.medium;
     }
 
     greater() {
-
+        this.min = this.medium;
     }
 }
 
